94bc221ac4be4e30950b73a75992aa3b9fc8d311
Your Name <your@email.com>
2021-12-03 23:45:53 -0500

    added move up command

--- client.js
+++ client.js
@@ -6,18 +6,26 @@ const connect = function () {
     host: 'localhost',
     port: 50541
   });
-  //use the .on method on our conn object to register a "connect" handler (a callback function)
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+   //use the .on method on our conn object to register a "connect" handler (a callback function)
   //In the callback, print a message to the screen when the connection is successfully established
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
   });
+
   //Send snakes name to server (SEN)
   conn.on('connect', () => {
     conn.write("Name: SEN");
+
   });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
   
+  // //Send a "Move up" command
+  // conn.on('connect', () => {
+  //   conn.write("Move: up");
+  // });
 
   return conn;
 };
