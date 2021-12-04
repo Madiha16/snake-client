5efd48593ad534b8b0013b74009ad5a67243c3d1
Your Name <your@email.com>
2021-12-04 15:57:11 -0500

    changes module.exports to ES6 shorthand

--- client.js
+++ client.js
@@ -10,30 +10,49 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-   //use the .on method on our conn object to register a "connect" handler (a callback function)
+  //use the .on method on our conn object to register a "connect" handler (a callback function)
   //In the callback, print a message to the screen when the connection is successfully established
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-  });
-
   //Send snakes name to server (SEN)
-  conn.on('connect', () => {
     conn.write("Name: SEN");
+  // //Send a "Move up" command
+  //   conn.write("Move: up");
 
+    //Successive move commands don't do anything?
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+
+  // //setInterval moves snake up off the board
+  //   setInterval(() => {
+  //     conn.write("Move: up");
+  //   }, 50);
+    
+    // //move snake up one after 1/2 sec
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 500);
+    
+    // //Moves snake to the left using setTimeout
+    // for (let i = 0; i <= 50; i++) {
+    //   setTimeout(() => {
+    //     conn.write("Move: left");
+    //   }, 500);
+    // }
+    
   });
   
-  // //Send a "Move up" command
-  // conn.on('connect', () => {
-  //   conn.write("Move: up");
-  // });
-
   return conn;
 };
 
 console.log("Connecting ...");
 
 //export the connect function
-module.exports = connect;
+module.exports = {
+  connect
+}
 
 // const myNumber = 42;
 // const myString = "hello";
