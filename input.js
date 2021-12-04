5248af32b51d1f0a99014de28600f1b02bc75492
Your Name <your@email.com>
2021-12-04 18:18:04 -0500

    added connection vairable to global scope to let stdin interect with data from keyboard

--- input.js
+++ input.js
@@ -1,6 +1,15 @@
-// // setup interface to handle user input from stdin
+// Stores the active TCP connection object.
+let connection;
+//connection should be in the outer-most scope so that it can be used by all functions in this module.
 
-const setupInput = function () {
+// setup interface to handle user input from stdin
+// added conn as a parameter into setupInput function
+//put connection = conn into setupInput function
+// connection = conn sets conn for the global scope so it can be used everywhere? ... can't just use conn?
+//had 2 snakes appearing because i didn't delete connect(?) from bottom of page
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,10 +20,24 @@ const setupInput = function () {
 };
 
 // //Added control+c input and terminate the game
+//after putting connection = conn in setupInnput
+//can use conn.write here
 const handleUserInput = function(key) {
   //console.log("the entered key is", key);// works!
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
